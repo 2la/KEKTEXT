98fbdcde348abf1e5d8adf6f8d8318d893608dde
2la <33842102+2la@users.noreply.github.com>
2020-11-28 19:20:23 +0300

    add docx export with highlighted text

--- doc_work/doc_work.py
+++ doc_work/doc_work.py
@@ -1,8 +1,14 @@
 import os
 import docx
-# import PyPDF2
 import pdf2docx
-from docx2python import docx2python
+
+
+def preprocess_text(text, remove_wrap=True, clear_paragraph=True):
+    if remove_wrap:
+        text = text.replace('-', '')
+    if clear_paragraph:
+        text = text.replace(' \n', '\n').replace('\n', ' ').replace('\t', '\n')
+    return text
 
 
 class Document:
@@ -10,12 +16,15 @@ class Document:
     Class 'Document' gets data_file_path
     Method 'parse' returns text from 'data_file_path' file
     """
-    def __init__(self, data_file_path, text_params):
+    def __init__(self, data_file_path, text_params={'start': '', 'stop': ''}):
         self.data_file_path = data_file_path
         self.filetype = data_file_path.rsplit('.')[-1]
         self.text_params = text_params
         self.text = ''
         self.docx = None
+        self.paragraphs = None
+        self.start_target_paragraph = None
+        self.stop_target_paragraph = None
 
     def pdf2docx(self):
         pdf2docx.parse(self.data_file_path, self.data_file_path + '.docx')
@@ -26,61 +35,60 @@ class Document:
     def docx_parser(self):
         doc = docx.Document(self.data_file_path)
         self.docx = doc
-        text = '\n'.join([paragraph.text for paragraph in doc.paragraphs])
-        self.text = text
-
-    # def pdf_parser(self):
-    #     doc = PyPDF2.PdfFileReader(open(self.data_file_path, 'rb'))
-    #     page = doc.getPage(0)
-    #     text = page.extractText().encode('utf-8')
-    #     self.text = text
+        self.paragraphs = [paragraph.text for paragraph in doc.paragraphs]
 
     def txt_parser(self):
         text = open(self.data_file_path, 'r', encoding='utf-8').read()
-        self.text = text
+        self.paragraphs = text.split('\n')
 
-    def parse(self, remove_wrap=True, clear_paragraph=True, ignore_text_params=False, void=True):
+    def parse(self, void=False):
+        # process doc
         if self.filetype == 'docx':
             self.docx_parser()
         elif self.filetype == 'pdf':
             self.pdf2docx()
             self.docx_parser()
-            # self.pdf_parser()
         elif self.filetype == 'txt':
             self.txt_parser()
-        if remove_wrap:
-            self.text = self.text.replace('-\n', '')
-        if clear_paragraph:
-            self.text = self.text.replace(' \n', '\n').replace('\n', ' ').replace('\t', '\n')
-        if not ignore_text_params:
-            p_start = self.text.find(self.text_params['start'])
-            p_stop = self.text.find(self.text_params['stop'])
-            print(f'p_start: {p_start}\np_stop:  {p_stop}')
-            self.text = self.text[p_start:p_stop]
+        # found target text (match start and stop paragraphs)
+        for i, paragraph in enumerate(self.paragraphs):
+            if self.start_target_paragraph is None:
+                if paragraph.find(self.text_params['start']) != -1:
+                    self.start_target_paragraph = i + 1
+            if self.stop_target_paragraph is None:
+                if paragraph.rfind(self.text_params['stop']) != -1:
+                    self.stop_target_paragraph = i
+        # extract text
+        self.paragraphs = self.paragraphs[self.start_target_paragraph: self.stop_target_paragraph]
+        self.paragraphs = list(map(preprocess_text, self.paragraphs))
+        self.text = '\n'.join(self.paragraphs)
         if void:
             return None
         else:
             return self.text
 
-    # def find_text(self):
-    #     self.parse()
-    #     p_start = self.text.find(self.text_params['start'])
-    #     p_stop = self.text.find(self.text_params['stop'])
-    #     print(f'p_start: {p_start}\np_stop:  {p_stop}')
-    #     return self.text[p_start:p_stop]
+    def change_text(self, new_text):
+        new_text = new_text.split('\n')
+        for i, text in zip(range(self.start_target_paragraph, self.stop_target_paragraph), new_text):
+            self.docx.paragraphs[i].text = text
+            for j in range(len(self.docx.paragraphs[i].runs)):
+                self.docx.paragraphs[i].runs[j].font.color.rgb = docx.shared.RGBColor(0x42, 0x24, 0xE9)
+        return None
 
-    def docx_writer(self, old_text, new_text, new_name):
-        p_start = self.text.find(self.text_params['start'])
-        p_stop = self.text.find(self.text_params['stop'])
-        self.docx
+    def save(self, new_name):
+        self.docx.save(new_name)
+        return None
 
 
 if __name__ == '__main__':
-    text_params = {'start': 'По прибытию на станцию Омск', #'307 УК РФ предупрежден',
-                   'stop': 'от 8 000 до 10 000 рублей' #'Перед началом, в ходе либо по окончании'
+    text_params = {'start': '307 УК РФ предупрежден',
+                   'stop': 'Перед началом, в ходе либо по окончании'
                    }
-    file_path = os.path.join('data', 'file3.pdf.docx')
+    cwd = ['data']
+    file_path = os.path.join(*cwd, 'Протокол-допроса-потерпевшего.docx')
     d = Document(file_path, text_params)
-    txt = d.parse(void=False, ignore_text_params=False)
-    # txt = d.find_text()
-    print(txt)
+    txt = d.parse()
+    new_txt = str.upper(txt)
+    d.change_text(new_txt)
+    d.save(os.path.join(*cwd, 'my_doc.docx'))
+    print(new_txt)
